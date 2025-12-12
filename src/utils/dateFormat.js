522db85ef03b80aec42679410327a5aaa2ff8ebe
Egar19 <madeeegar@gmail.com>
2025-12-12 14:43:39 +0800

    fix: remove manual +8 hour offset in date formatter

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -1,9 +1,7 @@
 export const dateFormat = (isoString) => {
   const date = new Date(isoString);
 
-  const witaDate = new Date(date.getTime() + 8 * 60 * 60 * 1000);
-
-  return witaDate.toLocaleString('id-ID', {
+  return date.toLocaleString('id-ID', {
     day: '2-digit',
     month: 'long',
     year: 'numeric',
